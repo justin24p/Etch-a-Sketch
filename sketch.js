6601199786bbf28d835cde545659865442e1042f
justinp24 <justinpardo2423@gmail.com>
2023-11-17 17:55:36 -0500

    create ui interface with html and css alongside button mode selection missing button logic

---
+++ sketch.js
@@ -0,0 +1,7 @@
+let userInput = 16
+let gridContainer = document.getElementById('gridContainer');
+
+for (let i = 0; i <= userInput; i ++) {
+    let gridElement = document.createElement('div');
+    gridContainer.appendChild(gridElement)
+}
