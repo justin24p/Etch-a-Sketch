6561babe4f07ed301df1928fe054d7b02dc42c65
justinp24 <justinpardo2423@gmail.com>
2023-11-18 15:02:52 -0500

    add color picker, grid size slider, rainbow mode

--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
-let userInput =  10
 let gridContainer = document.getElementById('gridContainer');
 let isMouseDown = false;
-let clearButton = document.getElementById('4');
 
+// buttons
+//let colorButton = document.getElementById('1');
+let rainButton = document.getElementById('2');
+let eraseButton = document.getElementById('3');
+let clearButton = document.getElementById('4');
+let colorPicker = document.getElementById('colorpicker');
+let slider = document.getElementById('slider');
 // Global scope event listeners to target mouse actions
+
 document.addEventListener("mousedown", () => {
     isMouseDown = true;
 });
@@ -12,38 +18,81 @@ document.addEventListener("mouseup", () => {
     isMouseDown = false;
 });
 
+let gridMode = "";
+
+// colorButton.addEventListener("click", () => {
+//     gridMode = "grey";
+// })
 
 
-for (let i = 0; i < userInput; i ++) {
-    let rowElement = document.createElement('div');
-    gridContainer.appendChild(rowElement);
+colorPicker.addEventListener("input", () => {
+    gridMode = colorPicker.value;
+})
 
-    for (let j = 0; j < userInput; j ++) {
-        let gridSize = 500 / userInput;
-        let gridElement = document.createElement('div');
+eraseButton.addEventListener("click", () => { 
+    gridMode = "rgb(245, 233, 217)";
+})
 
-        gridElement.style.width = gridSize +  'px';
-        gridElement.style.height = gridSize + 'px';
+let rainbow = rainButton.addEventListener("click", () => {
+    gridMode = "rainbow";
+})
 
-        rowElement.appendChild(gridElement)
-
-
-        gridElement.addEventListener("mousemove", (event) => {
-            if(isMouseDown) {
-                gridElement.style.backgroundColor = 'grey';
-            } 
-        }); 
-    }
-    gridContainer.appendChild(rowElement);
+function determineMode() { 
+    return gridMode;
 }
 
-//clear grid function
+//
+let userInput =  10
+
+function creategrid() {
+    for (let i = 0; i < userInput; i ++) {
+        let rowElement = document.createElement('div');
+        gridContainer.appendChild(rowElement);
+
+        for (let j = 0; j < userInput; j ++) {
+            let gridSize = 500 / userInput;
+            let gridElement = document.createElement('div');
+
+            gridElement.style.width = gridSize +  'px';
+            gridElement.style.height = gridSize + 'px';
+
+            rowElement.appendChild(gridElement)
+
+
+            gridElement.addEventListener("mouseover", (event) => {
+                //
+                //
+                if(isMouseDown && gridMode !== "rainbow") { 
+                    gridElement.style.backgroundColor = determineMode();
+                } 
+                else if (isMouseDown && gridMode === "rainbow") {
+                    let colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
+                    let random = colors[Math.floor(Math.random() * 7)];
+                    gridElement.style.backgroundColor = random;
+                }
+            }); 
+        }
+        gridContainer.appendChild(rowElement);
+    }
+}
+
+// default size
+creategrid(userInput);
+
+slider.addEventListener("input", () => {
+    let sliderValue = parseInt(slider.value, 10);
+    userInput = sliderValue
+    gridContainer.innerHTML = '';
+    creategrid();
+})
+
+
+// //clear grid function
 clearButton.addEventListener("click", () => {
     let gridElements = gridContainer.querySelectorAll('div');
 
     gridElements.forEach((gridElement) => {
         gridElement.style.backgroundColor = "";
     });
-    
 });
 
