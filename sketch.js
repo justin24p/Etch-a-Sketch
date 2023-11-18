80271b5814b504e3a3e6273eece80620e5f4d47a
justinp24 <justinpardo2423@gmail.com>
2023-11-17 23:41:50 -0500

    implement grid coloring tool to ui alongside ability to clear canvas

--- sketch.js
+++ sketch.js
@@ -1,7 +1,49 @@
-let userInput = 16
+let userInput =  10
 let gridContainer = document.getElementById('gridContainer');
+let isMouseDown = false;
+let clearButton = document.getElementById('4');
 
-for (let i = 0; i <= userInput; i ++) {
-    let gridElement = document.createElement('div');
-    gridContainer.appendChild(gridElement)
+// Global scope event listeners to target mouse actions
+document.addEventListener("mousedown", () => {
+    isMouseDown = true;
+});
+
+document.addEventListener("mouseup", () => {
+    isMouseDown = false;
+});
+
+
+
+for (let i = 0; i < userInput; i ++) {
+    let rowElement = document.createElement('div');
+    gridContainer.appendChild(rowElement);
+
+    for (let j = 0; j < userInput; j ++) {
+        let gridSize = 500 / userInput;
+        let gridElement = document.createElement('div');
+
+        gridElement.style.width = gridSize +  'px';
+        gridElement.style.height = gridSize + 'px';
+
+        rowElement.appendChild(gridElement)
+
+
+        gridElement.addEventListener("mousemove", (event) => {
+            if(isMouseDown) {
+                gridElement.style.backgroundColor = 'grey';
+            } 
+        }); 
+    }
+    gridContainer.appendChild(rowElement);
 }
+
+//clear grid function
+clearButton.addEventListener("click", () => {
+    let gridElements = gridContainer.querySelectorAll('div');
+
+    gridElements.forEach((gridElement) => {
+        gridElement.style.backgroundColor = "";
+    });
+    
+});
+
